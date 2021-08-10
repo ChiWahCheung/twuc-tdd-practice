08b37272675717a68ffcf758345b932827c000a5
ChiWahCheung <kokozz@sina.cn>
2021-08-10 11:01:07 +0800

    [Zhihua Zhang] should create Mommifier method

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.Test;
+
 public class MommifierTest {
+
+    /*
+     * 1.
+     * Given:
+     * empty string
+     * When:
+     * convert
+     * Then:
+     * itself
+     */
+
+    @Test
+    void should_create_method() {
+        new Mommifier();
+    }
+
+    @Test
+    void should_return_empty_string_when_given_empty_string() {
+
+    }
 }
