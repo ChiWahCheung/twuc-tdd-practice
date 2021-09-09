0406279d9cbd561988e4b923e518e34cf1889f46
Zhihua Zhang <zhihua.zhang@thoughtworks.com>
2021-09-09 13:51:05 +0800

    [Zhihua Zhang] feat + refactor: should return covert continuous vowels to mommy given the number of vowels more than 30%.

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,5 +1,41 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Mommifier {
     public String covert(String s) {
+        if (s.length() == 0) {
+            return s;
+        }
+        List<String> vowels = Arrays.asList("a", "o", "e", "i", "u");
+        StringBuilder result = new StringBuilder();
+        double vowelQuantity = getVowelQuantity(s, vowels, result);
+        double vowelsProportion = vowelQuantity / s.length();
+        return selectLegalResult(s, result, vowelsProportion);
+    }
+
+    private String selectLegalResult(String s, StringBuilder result, double vowelsProportion) {
+        if (vowelsProportion >= 0.3) {
+            return result.toString();
+        }
         return s;
+    }
+
+    private double getVowelQuantity(String s, List<String> vowels, StringBuilder result) {
+        double vowelQuantity = 0;
+        boolean isContinuousVowel = false;
+        for (int i = 0; i < s.length(); i++) {
+            String letterInS = Character.toString(s.charAt(i));
+            if (vowels.contains(letterInS)) {
+                if (!isContinuousVowel) {
+                    result.append("mommy");
+                    isContinuousVowel = true;
+                }
+                vowelQuantity++;
+            } else {
+                isContinuousVowel = false;
+                result.append(letterInS);
+            }
+        }
+        return vowelQuantity;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -14,10 +14,6 @@ public class MommifierTest {
      * itself
      */
     Mommifier mommifier = new Mommifier();
-    @Test
-    void should_create_method() {
-        new Mommifier();
-    }
 
     @Test
     void should_return_empty_string_when_given_empty_string() {
@@ -27,5 +23,25 @@ public class MommifierTest {
     @Test
     void should_return_itself_given_not_vowels() {
         assertEquals("b", mommifier.covert("b"));
+    }
+
+    @Test
+    void should_return_mommy_given_one_vowel() {
+        assertEquals("mommy", mommifier.covert("a"));
+    }
+
+    @Test
+    void should_return_itself_given_the_number_of_vowels_less_than_30percent() {
+        assertEquals("abbbbbb", mommifier.covert("abbbbbb"));
+    }
+
+    @Test
+    void should_return_covert_singele_not_continuous_vowel_to_mommy_given_the_number_of_vowels_more_than_30percent() {
+        assertEquals("mommybcdmommy", mommifier.covert("abcde"));
+    }
+
+    @Test
+    void should_return_covert_continuous_vowels_to_mommy_given_the_number_of_vowels_more_than_30percent() {
+        assertEquals("mommybcdmommy", mommifier.covert("aoebcdiu"));
     }
 }
